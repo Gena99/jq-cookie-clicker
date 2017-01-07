fe0502fbad00b1262e50eee9f5e7118288f24b27
Gena <gena@e64.net>
2017-01-07 23:11:10 +0100

    compteur avec alert tous les 20 click

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	//votre code
 	var compteur = 0;
 	$('.cake').click(function(){
-		
 		compteur++;
 		console.log(compteur);
 		$('.counter').text(compteur);
+		if(compteur%20 == 0){
+			alert('Rien de mieux à faire ?');
+		}
 	});
 });
