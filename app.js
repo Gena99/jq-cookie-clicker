f55dfafcf1412cf0755a899e3fec3237f1c217ce
Gena <gena@e64.net>
2017-01-07 22:53:39 +0100

    exercice cookie compteur

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	//votre code
-
-
+	var compteur = 0;
+	$('.cake').click(function(){
+		
+		compteur++;
+		console.log(compteur);
+		$('.counter').text(compteur);
+	});
 });
